37569dcbe61d3621352bd75dabb281af5a935861
mrchuvyzgalov <mr.chuvyzgalov@gmail.com>
2024-03-19 18:41:00 +0100

    HW06 solution

--- monads/src/Either.hs
+++ monads/src/Either.hs
@@ -6,10 +6,100 @@ data MyEither a b
   deriving (Show, Eq)
 
 -- Implement the instance and prove the laws
-instance Functor (MyEither a)
+instance Functor (MyEither a) where
+    fmap _ (MyLeft x) = MyLeft x
+    fmap f (MyRight x) = MyRight (f x)
+
+-- Proof:
+-- 1. fmap id == id
+--    
+--    1.1. 
+--        a) fmap id Left x = Left x
+--        b) id Left x = Left x
+--        c) (a) == (b) - PROVED
+--    
+--    1.2. 
+--        a) fmap id Right x = Right (id x) = Right x
+--        b) id Right x = Right x
+--        c) (a) == (b) - PROVED
+--
+--    
+-- 2. fmap (f . g) == (fmap f) . (fmap g) 
+--        
+--    2.1.
+--        a) fmap (f . g) Left x = Left x
+--        b) (fmap  f) . (fmap g) Left x = (fmap f) (fmap g Left x) = fmap f Left x = Left x
+--        c) (a) == (b) - PROVED
+--     
+--    2.2.
+--        a) fmap (f . g) Right x = Right (f . g) x 
+--        b) (fmap f) . (fmap g) Right x = (fmap f) (fmap g Right x) = fmap f Right (g x) = Right (f (g x)) = Right (f . g x) 
+--        c) (a) == (b) - PROVED
+-- 
+-- 3. 1 - PROVED, 2 - PROVED |-> Functor Either - PROVED
 
 -- Implement the instance and prove the laws
-instance Applicative (MyEither a) 
+instance Applicative (MyEither a) where
+  pure = MyRight
+  MyLeft f <*> _ = MyLeft f
+  MyRight f <*> MyRight x = MyRight (f x)
+  MyRight _ <*> MyLeft x = MyLeft x
+
+-- Proof:
+-- 1. Identity
+--    a) pure id <*> MyRight x = MyRight id <*> MyRight x =  MyRight (id x) = MyRight x - PROVED
+--    b) pure id <*> MyLeft x = MyRight id <*> MyLeft x = MyLeft x - PROVED
+--
+-- 2. Composition    
+--    a) pure (.) <*> u <*> v <*> w = MyRight (.) <*> MyRight x <*> MyRight y <*> w = 
+--       = MyRight (. x) <*> MyRight y <*> w = MyRight (x . y) <*> w = 
+--
+--       a.1) = MyRight (x . y) <*> MyRight z = MyRight ((x . y) z)
+--       a.2) = MyRight (x . y) <*> MyLeft z = MyLeft z
+--
+--    b) u <*> (v <*> w) = MyRight x <*> (MyRight y <*> w) =
+--       
+--       b.1) = MyRight x <*> (MyRight y <*> MyRight z) = MyRight x <*> MyRight (y z) = MyRight ((x .y) z)
+--       b.2) = MyRight x <*> (MyRight y <*> MyLeft z) = MyRight x <*> MyLeft z = MyLeft z
+--
+--    if u or v is MyLeft => Result is MyLeft
+--
+-- 3. Homomorphism
+--    a) pure f <*> pure x = MyRight f <*> MyRight x = MyRight (f x)
+--    b) pure (f x) = MyRight (f x)
+-- 
+-- 4. Interchange
+--    a) u <*> pure y = u <*> MyRight y =
+-- 
+--       a.1) = MyRight x <*> MyRight y = MyRight (x y)
+--       a.2) = MyLeft x <*> MyRight y = MyLeft x
+-- 
+--    b) pure ($ y) <*> u = MyRight ($ y) <*> u = 
+--
+--       b.1) = MyRight ($ y) <*> MyRight x = MyRight (x y)
+--       b.2) = MyRight ($ y) <*> MyLeft x = MyLeft x 
 
 -- Implement the instance and prove the laws
-instance Monad (MyEither a)
+instance Monad (MyEither a) where
+  return = pure
+  (>>=) (MyRight x) f = f x
+  (>>=) (MyLeft x) _ = MyLeft x
+
+-- Proof:
+-- 1. Identity
+--    a) return a >>= k = MyRight a >>= k = k a - PROVED 
+--    b) m >>= return = 
+--       
+--      b.1) = MyRight x >>= return = return x = MyRigth x
+--      b.2) = MyLeft x >>= return = MyLeft x
+--
+-- 2. Assiciativity    
+--    a) m >>= (\x -> k x >>= h) =
+--
+--      a.1) = MyRight z >>= (\x -> k x >>= h) = (k z) >>= h = h (k z)
+--      a.2) = MyLeft z >>= (\x -> k x >>= h) = MyLeft z
+--    
+--    b) (m >>= k) >>= h
+--     
+--      b.1) = (MyRight z >>= k) >>= h = (k z) >>= h = h (k z)
+--      b.2) = (MyLeft z >>= k) >>= h = MyLeft z >>= h = MyLeft z
--- monads/src/Logger.hs
+++ monads/src/Logger.hs
@@ -4,12 +4,62 @@ data Logger l a = Logger [l] a deriving (Show, Eq)
 
 -- Implement the instance and prove the laws
 instance Functor (Logger l) where 
+    fmap f (Logger logs a) = Logger logs (f a)
+
+-- Proof:
+-- 1. Identity
+--    a) fmap id (Logger logs a) = Logger logs (id a) = Logger logs a
+--    b) id (Logger logs a) = Logger logs a
+--       
+--
+-- 2. Composition  
+--    a) fmap (f . g) (Logger logs a) = Logger logs ((f . g) a)
+--    b) (fmap f) . (fmap g) (Logger logs a) = (fmap f) (Logger logs (g a)) = Logger logs ((f .g) a)
+
 
 -- Implement the instance and prove the laws
 instance Applicative (Logger l) where 
+    pure = Logger []
+    (Logger f_logs f) <*> (Logger logs x) = Logger (f_logs ++ logs) (f x)
+
+-- Proof:
+-- 1. Identity
+--    pure id <*> Logger logs x = Logger [] id <*> Logger logs x = Logger logs (id x) = Logger logs x
+--
+-- 2. Composition    
+--    a) pure (.) <*> u <*> v <*> w = Logger [] (.) <*> Logger x_logs x <*> Logger y_logs y <*> Logger z_logs z = 
+--       = Logger x_logs (. x) <*> Logger y_logs y <*> Logger z_logs z = Logger (x_logs ++ y_logs) (x . y) <*> Logger z_logs z
+--       = Logger (x_logs ++ y_logs ++ z_logs) ((x . y) z)
+--
+--    b) u <*> (v <*> w) = Logger x_logs x <*> ((Logger y_logs y) <*> Logger z_logs z) =
+--       = Logger x_logs x <*> (Logger (y_logs ++ z_logs) (y z) = Logger (x_logs ++ y_logs ++ z_logs) ((x . y) z) 
+--
+-- 3. Homomorphism
+--    a) pure f <*> pure x = Logger [] f <*> Logger [] x = Logger [] (f x)
+--    b) pure (f x) = Logger [] (f x)
+-- 
+-- 4. Interchange
+--    a) u <*> pure y = Logger x_logs x <*> Logger [] y = Logger x_logs (x y)
+--    b) pure ($ y) <*> u = Logger [] ($ y) <*> Logger x_logs x = Logger x_logs (x y)
 
 -- Implement the instance and prove the laws
 instance Monad (Logger l) where 
+    return = pure
+    (>>=) (Logger logs x) f = 
+        case f x of
+            Logger new_logs y -> Logger (logs ++ new_logs) y
+
+-- Proof:
+-- 1. Identity
+--    a) return a >>= k = Logger [] x >>= k = k x 
+--    b) m >>= return = Logger x_logs x >>= return = Logger x_logs x
+--
+-- 2. Assiciativity    
+--    a) m >>= (\x -> k x >>= h) = Logger y_logs y >>= (\x -> k x >>= h) = Logger (y_logs ++ k_logs) (k y) >>= h
+--       = Logger (y_logs ++ k_logs ++ h_logs) ((h . k) y)
+--    b) (m >>= k) >>= h = (Logger y_logs y >>= k) >>= h = (Logger (y_logs ++ k_logs) (k y)) >>= h
+--       = Logger (y_logs ++ k_logs ++ h_logs) ((h . k) y)
+--     
 
 -- Writes a single log message. 
 -- Can be easily bound together with other logging computations.
